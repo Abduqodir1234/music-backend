f7594dda3c2ac6434f8998d89b3220b3d2a19d34
Abduqodir1234 <gofurboyevabduqodir@gmail.com>
2021-07-29 04:00:29 +0500

    Abduqodir

--- music/urls.py
+++ music/urls.py
@@ -13,19 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('app.urls'))
+    path('api/', include('app.urls'))
 ]
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    # serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# serve static and media files from development server
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
